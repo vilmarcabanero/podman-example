b5e063175c6e8e62fcfe53b44345e6af13b4a247
Vilmar Cabañero <vilmarcabanero@gmail.com>
2023-03-20 18:22:47 +0800

    commit

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.send('<h1>Hello World!</h1>'))
+
+app.listen(port, () => console.log(`My server listening at http://localhost:${port}`))
